0d45579828e24bd93d3680f5e571f1e173cf0db2
Yevgen Gugel <148051488+trajil@users.noreply.github.com>
2024-09-24 01:18:37 +0200

    comment_remover.c working

---
+++ learning_with_dr_chuck/tasks chapter 01/comment-modified.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+
+int main() {
+    int a = 5; 
+    
+    char c = 'a'; 
+    
+    char quote_in_char = '\''; 
+    
+    printf("Hello, World!\n"); 
+    
+    printf("This string has a // fake comment inside it.\n");
+    printf("This string has a // fake comment inside it.\n");
+    printf("This string has a // fake comment inside it.\n");
+    
+    printf("This string has a /* fake multi-line comment */ inside it.\n");
+    
+    
+    
+   
+    
+   
+   
+    printf("Valid code here\n");
+
+    return 0;
+}
--- learning_with_dr_chuck/tasks chapter 01/comment_remover.c
+++ learning_with_dr_chuck/tasks chapter 01/comment_remover.c
@@ -6,36 +6,100 @@
 
 int main(int argc, char const *argv[])
 {
+
     int i = 0;
-    int c, counter = 0;
+    int c;
+    int counter = 0;
     char filea_cache[LENGTH];
+    char filec_cache[LENGTH];
     FILE *filea;
     FILE *fileb;
+    FILE *filec;
 
-    filea = fopen("test.txt", "r");
-    fileb = fopen("test-output.txt", "w");
 
+    filea = fopen(argv[1], "r");
+    fileb = fopen("datei-unmodified.txt", "w");
+    filec = fopen(argv[2], "w");
+
+    // reading source file
     if (filea)
     {
-        counter = 0;
-        // filling internal char array
         while ((c = getc(filea)) != EOF)
         {
             filea_cache[counter] = c;
+            fprintf(fileb, "%c", c);
             counter++;
         }
         filea_cache[counter] = '\0';
-        printf("output:%s", filea_cache);
     }
+    counter = 0;
+    //printf("filea_cache before modifications:\n%s\n", filea_cache);
 
-    while ((c = getc(filea_cache)) != EOF)
+    int counter_c = 0, in_comment = 0, in_string = 0;
+    // modifying the cache
+    while (((c = filea_cache[counter]) != '\0'))
     {
+        if ((c = filea_cache[counter]) == '"' && in_string == 0)
+        {
+            in_string = 1;
+            filec_cache[counter_c++] = '"';
+            counter++;
+        }
+        if (((c = filea_cache[counter]) == '"' && in_string == 1))
+        {
+            in_string = 0;
+        }
 
-        fprintf(fileb, "TEST");
+        // single line comment remover
+        if ((filea_cache[counter] == '/') && (filea_cache[counter + 1] == '/') && (in_string == 0))
+        {
+            in_comment = 1;
+            while (in_comment)
+            {
+                if ((c = filea_cache[counter]) == '\n')
+                {
+                    in_comment = 0;
+                }
+
+                counter++;
+            }
+            filec_cache[counter_c - 1] = '\n';
+        }
+
+        // multi line comment remover
+        if ((filea_cache[counter] == '/') && (filea_cache[counter + 1] == '*') && (in_string == 0))
+        {
+            in_comment = 1;
+            while (in_comment)
+            {
+                if (filea_cache[counter] == '*' && filea_cache[counter + 1] == '/')
+                {
+                    in_comment = 0;
+                    counter++;
+                }
+
+                counter++;
+            }
+        }
+
+        filec_cache[counter_c] = filea_cache[counter];
+
+        counter_c++;
+        counter++;
     }
-    fprintf(fileb, "\0");
+    filec_cache[counter_c] = '\0';
+    counter = 0;
+    //printf("filec_cache after modifications:\n%s", filec_cache);
 
+    // writing into output file
+    while (((c = filec_cache[counter]) != '\0'))
+    {
+        fprintf(filec, "%c", c);
+        counter++;
+    }
+    printf("file: %s created\n", argv[2]);
     fclose(filea);
     fclose(fileb);
+    fclose(filec);
     return 0;
 }
