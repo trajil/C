4ff98a368324c11a7d673cefd928344e27b5635a
Yevgen Gugel <148051488+trajil@users.noreply.github.com>
2024-09-27 16:14:00 +0200

    added line counting mechanism

--- learning_with_dr_chuck/tasks chapter 01/code_checker.c
+++ learning_with_dr_chuck/tasks chapter 01/code_checker.c
@@ -3,10 +3,31 @@
 // Exercise 1-23. Write a program to check a C program for rudimentary syntax errors like unbalanced parentheses, brackets and braces. Don't forget about quotes, both single and double, and comments. (This program is hard if you do it in full generality.)
 #define LENGTH 65536
 #define STACKSIZE 4096
+#define LINES 65536
 
-void pop(char stack[], int j, int *succesfully_deleted, int *current_stack_length)
+int which_line(int lines[LINES], int *counter)
 {
-    printf("enter pop\n");
+    int in_line = 1, index = 0, counting = 1;
+    int position = *counter;
+
+    while (counting == 1 && index < LINES)
+    {
+        if (position >= lines[index] && position <= lines[index + 1])
+        {
+            counting = 0;
+        }
+        if (counting == 1)
+        {
+            in_line++;
+            index += 2;
+        }
+    }
+
+    return in_line;
+}
+
+void pop(char stack[], int stack_address[], int j, int *succesfully_deleted, int *current_stack_length)
+{
     // Check if the index `j` is valid
     if (j < 0 || j >= *current_stack_length)
     {
@@ -18,7 +39,7 @@ void pop(char stack[], int j, int *succesfully_deleted, int *current_stack_lengt
     for (int i = j; i < *current_stack_length - 1; i++)
     {
         stack[i] = stack[i + 1];
-        printf("deleted from stack\n");
+        stack_address[i] = stack_address[i + 1];
     }
 
     *current_stack_length -= 1;          // Reduce the stack length by 1
@@ -26,11 +47,11 @@ void pop(char stack[], int j, int *succesfully_deleted, int *current_stack_lengt
     *succesfully_deleted = 1;            // Mark successful deletion
 }
 
-void handle_stack(char c, char stack[], int *current_stack_length, char chars_to_check[], int *right_bracket_error, int *position_of_bracket)
+void handle_stack(char c, char stack[], int stack_address[], int *current_stack_length, char chars_to_check[], int *right_bracket_error, int *position_of_bracket, int *counter)
 {
-    int i = *position_of_bracket, found_char = 0, succesfully_deleted = 0;
-    //printf("%d\n",i);
-    // whether or not it's: ), ] or }
+    int i = *position_of_bracket, succesfully_deleted = 0;
+    // printf("%d\n",i);
+    //  whether or not it's: ), ] or }
     if (i % 2 == 1)
     {
         // setting flag, waiting to find an opening bracket to match a pending closing bracket
@@ -40,8 +61,10 @@ void handle_stack(char c, char stack[], int *current_stack_length, char chars_to
         {
             if (stack[j] == chars_to_check[i - 1])
             {
-                pop(stack, j, &succesfully_deleted, current_stack_length);
+                pop(stack, stack_address, j, &succesfully_deleted, current_stack_length);
                 *right_bracket_error = 0;
+                printf("deleted from stack\n");
+                printf("address: %d\n", stack_address[*current_stack_length]);
                 break; // Exit the loop once the counterpart is found and deleted.
             }
         }
@@ -51,7 +74,9 @@ void handle_stack(char c, char stack[], int *current_stack_length, char chars_to
     else
     {
         stack[*current_stack_length] = c;
-        printf("added to stack\n");
+        stack_address[*current_stack_length] = *counter;
+        printf("added to stack!\n");
+        printf("address: %d\n", stack_address[*current_stack_length]);
         (*current_stack_length)++;
         stack[*current_stack_length] = '\0';
     }
@@ -97,18 +122,27 @@ void handle_comments(int *counter, char file_cache[])
     }
 }
 
-void write_to_array(FILE *file, char file_cache[])
+void write_to_array(FILE *file, char file_cache[], int lines[])
 {
-    if (file)
+    int d, i = 0, j = 0, start_of_line = 1;
+    while ((d = getc(file)) != EOF && i < LENGTH - 1)
     {
-        int d, i = 0;
-        while ((d = getc(file)) != EOF && i < LENGTH - 1)
+        file_cache[i] = d;
+        if (start_of_line == 1)
         {
-            file_cache[i] = d;
-            i++;
+            lines[j] = i;
+            start_of_line = 0;
+            j++;
         }
-        file_cache[i] = '\0';
+        if (start_of_line == 0 && d == '\n')
+        {
+            lines[j] = i;
+            start_of_line = 1;
+            j++;
+        }
+        i++;
     }
+    file_cache[i] = '\0';
 }
 
 void set_stack_limit(char stack[], int stack_limiter)
@@ -123,7 +157,7 @@ void is_in_chars_to_check(int *revelant_syntax, char c, char chars_to_check[], i
     {
         if (c == chars_to_check[i])
         {
-            
+
             b = 1;
             break;
         }
@@ -136,7 +170,9 @@ int main(int argc, char const *argv[])
 {
     char filea_cache[LENGTH];
     char stack[STACKSIZE];
-    int c, counter_stack = 0, counter = 0, current_stack_length = 0, line_counter = 1, position_of_bracket = 0;
+    int stack_address[STACKSIZE];
+    int lines[LINES];
+    int c, counter_stack = 0, counter = 0, current_stack_length = 0, in_line = 0, position_of_bracket = 0;
     int in_comment = 0, relevant_syntax = 0, right_bracket_error = 0;
     char chars_to_check[7] = {'(', ')', '[', ']', '{', '}', '\0'};
     chars_to_check[6] = '\0';
@@ -150,25 +186,20 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-    write_to_array(filea, filea_cache);
+    write_to_array(filea, filea_cache, lines);
     fclose(filea);
     set_stack_limit(stack, current_stack_length);
     // printf("filea_cache after writing:\n%s", filea_cache);
 
     while ((c = filea_cache[counter]) != '\0' && counter < LENGTH - 1 && right_bracket_error == 0)
     {
-        printf("c is %c [%d]\n", c, counter);
         if (c == '"' || c == '\'')
         {
-            printf("quotes in\n");
             handle_quotes(c, &counter, filea_cache);
-            printf("quotes out\n");
         }
         if (c == '/')
         {
-            printf("comment int\n");
             handle_comments(&counter, filea_cache);
-            printf("comment out\n");
         }
 
         c = filea_cache[counter];
@@ -179,9 +210,9 @@ int main(int argc, char const *argv[])
 
         if (relevant_syntax == 1)
         {
-            printf("stack in\n");
-            handle_stack(c, stack, &current_stack_length, chars_to_check, &right_bracket_error, &position_of_bracket);
-            printf("stack out\n");
+            printf("c is %c [%d]\n", c, counter);
+            handle_stack(c, stack, stack_address, &current_stack_length, chars_to_check, &right_bracket_error, &position_of_bracket, &counter);
+            printf("line: %d\n", in_line = which_line(lines, &counter));
         }
 
         counter++;
--- learning_with_dr_chuck/tasks chapter 01/comment1.c
+++ learning_with_dr_chuck/tasks chapter 01/comment1.c
@@ -1,4 +1,5 @@
 int main(int argc, char const *argv[])
 { 
+    // () [{}]
     return 0;
 }
